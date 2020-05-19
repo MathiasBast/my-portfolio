0326fa397c901b19f915bf050923000d0400cd5d
Mathias Bast <mathiasbast123@gmail.com>
2020-05-20 08:43:10 +1200

    adds home paragraphs

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,8 +4,26 @@ const Home = () => {
   return (
     <>
       <div className='home-div' >
-        <h1>Mathias Bast</h1>
+        <h1>Mathias Sebastian Bast</h1>
         <h2>Web Developer</h2>
+      </div>
+      <div className='home-me' >
+        <p>Hello, my name us Mathias</p>
+        <p>
+          I do love coding and learning new things, I think I love it because of the satisfaction of putting it to use at the end, 
+          and once I understand something I do have an urge to try to understand how it works. I think this does make me a good learner 
+          who can pick things up easily who is easily motivated to work projects to completion.
+        </p>
+        <p>
+          I attended EDA (Enspiral Dev Academy) which is a boot camp. I went to the Auckland campus but due to the COVID-19 pandemic, I did
+          complete most of it online. In the last weeks of EDA, we did have simulations of work environments with agile practices and scrum roles,
+          and a very high code quality. EDA did emphasize testing with Jest and I did learn about Enzyme along the way. From the boot camp, I did
+          learn a lot about working remotely effectively and how to teach myself skills, I think this is very valuable in a modern work environment.
+          To work remotely I do need to be motivated, this can be by the work I am doing or a team around me. EDA did also teaches about people
+          skills, like empathy, feedback, conflict resolution, and more. These may sound simple but they taught how to implement them in a work
+          environment while keeping a team working together productively and I have used them in my personal life a few times because of how
+          useful I find them.
+        </p>
       </div>
     </>
   )
