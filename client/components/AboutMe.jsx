001b7a17f0bc88f652b3b21e3c2b1355afce01b0
Mathias Bast <mathiasbast123@gmail.com>
2020-05-22 08:07:51 +1200

    adds paragraph

--- client/components/AboutMe.jsx
+++ client/components/AboutMe.jsx
@@ -7,8 +7,13 @@ const AboutMe = () => {
       <h1>Mathias Sebastian Bast</h1>
       <h2>About Me</h2>
     </div>
-    <div>
-      <p>Hiiii</p>
+    <div className='home-me' >
+      <p>My name is Mathias Sebastian Peter Bast</p>
+      <p>
+        I was born on March 10th, 2002 in West Auckland in a town called Laingholm. I did stay in Auckland until late 2017. Then I did move to
+        Gisborne on the East Coast where I did finish college and further my interest in technology. I did get recommended a code Bootcamp named
+        Enspiral Dev Academy.
+      </p>
     </div>
     </>
   )
