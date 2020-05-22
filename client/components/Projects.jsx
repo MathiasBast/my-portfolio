19a9ba74134ddccaa8655e8179393592699d5cc3
Mathias Bast <mathiasbast123@gmail.com>
2020-05-22 15:18:58 +1200

    added full stop

--- client/components/Projects.jsx
+++ client/components/Projects.jsx
@@ -8,12 +8,21 @@ const Projects = () => {
         <h2>My Projects</h2>
       </div>
       <div className='project-container' >
-        <a href='https://take-me-nz.herokuapp.com/' style={{ textDecoration: 'none' }} ><div className='project-item' >
-          <h2>Take Me</h2>
-          <img className='take-me' src='/images/TakeMe.png' />
-          <p>Take Me is a group project I did work on. I did do it while I was at EDA and was my final project</p>
-        </div></a>
-        <div className='project-item' ></div>
+        <a href='https://take-me-nz.herokuapp.com/' style={{ textDecoration: 'none' }} >
+          <div className='project-item' >
+            <h2>Take Me</h2>
+            <img className='take-me' src='/images/TakeMe.png' />
+            <p>Take Me is a group project I did work on. I did do it while I was at EDA and was my final project</p>
+          </div>
+        </a>
+
+        <a href='https://mars-pic.herokuapp.com/'style={{ textDecoration: 'none' }} >
+          <div className='project-item' >
+            <h2>Mars Pic</h2>
+            <img className='mars-pic' src='/images/MarsPic.jpg' />
+            <p>An app that uses nasas rover api. See pictures from Mars.</p>
+          </div>
+        </a>
         <div className='project-item' ></div>
       </div>
     </>
