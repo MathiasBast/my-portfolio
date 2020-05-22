3ab3e9112be2c1fef6cbb6f6dbfd24076811d53b
Mathias Bast <mathiasbast123@gmail.com>
2020-05-22 13:35:45 +1200

    its basic

--- client/components/ContactMe.jsx
+++ client/components/ContactMe.jsx
@@ -8,13 +8,29 @@ const ContactMe = () => {
         <h2>Contact Me</h2>
       </div>
       <div className='home-me' >
-        <img className='contact-logos' src='/images/GitHub.png' />
-        <a href='https://github.com/MathiasBast' ><p>GitHub</p></a>
-        <img className='contact-logos' src='/images/Linkedin.png' />
-        <a href='https://www.linkedin.com/in/mathias-bast-5682341aa/' ><p>LinkedIn</p></a>
-        <img className='contact-logos' src='/images/Email.png' />
-        <a href='mailto: mathiasbast123@gmail.com' ><p>Email Me</p></a><p> or mathiasbast123@gmail.com</p>
-        <p>Phone Number: 021 138 0545</p>
+        <div className='contact-container' >
+          <a href='https://github.com/MathiasBast' >
+            <div className='contact-item' >
+              <img className='contact-logos' src='/images/GitHub.png' />
+              <p>GitHub</p>
+            </div>
+          </a>
+          <a href='https://www.linkedin.com/in/mathias-bast-5682341aa/' >
+            <div className='contact-item' >
+              <img className='contact-logos' src='/images/Linkedin.png' />
+              <p>LinkedIn</p>
+            </div>
+          </a>
+          <div className='contact-item' >
+            <img className='contact-logos' src='/images/Email.png' />
+            <a href='mailto: mathiasbast123@gmail.com' ><p>Email Me</p></a><p className='contact-info' >mathiasbast123@gmail.com</p>
+          </div>
+          <div className='contact-item' >
+            <img className='contact-logos' src='/images/Phone.png' />
+            <p style={{ fontWeight: 'bold' }} >Phone Number:</p>
+            <p> 021 138 0545</p>
+          </div>
+        </div>
       </div>
     </>
   )
